bb5342e5bdfafa6ffa331557ab640c10f4f2319c
somkiat lee <somkarun@gmail.com>
2021-05-09 11:19:14 +0700

    Update start.py blink
    blink led update in start.py

--- app/start.py
+++ app/start.py
@@ -1,1 +1,18 @@
-print('Version 2 installed using USB V2') 
+from machine import Pin
+from time import sleep
+
+print('Version 2 installed using USB V2')
+
+led = Pin(2, Pin.OUT)
+
+while True:
+    led.value(1)
+    sleep(0.4)
+    led.value(0)
+    sleep(0.4)
+    led.value(1)
+    sleep(0.4)
+    led.value(0)
+    sleep(0.4)
+    led.value(1)
+    sleep(2)
